681eab45b1e8d81e06480184faf4cd462198ea3b
Mac Heller-Ogden <mac@machellerogden.com>
2014-11-25 16:53:38 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,80 @@
+var fs = require('fs'),
+    path = require('path'),
+    through = require('through2'),
+    gutil = require('gulp-util'),
+    PluginError = gutil.PluginError,
+    exec = require('child_process').exec,
+    os = require('os'),
+    format = require('util').format;
+
+module.exports = function (options) {
+    var SONAR_RUNNER_HOME,
+        JAR,
+        SONAR_RUNNER_COMMAND,
+        write,
+        flush;
+
+    SONAR_RUNNER_HOME = path.join(__dirname, '/sonar-runner-2.3');
+    JAR = '/lib/sonar-runner-dist-2.3.jar';
+    SONAR_RUNNER_COMMAND = 'java -jar ' + path.join(SONAR_RUNNER_HOME, JAR) + ' -X -Drunner.home=' + SONAR_RUNNER_HOME;
+
+    write = function (file, enc, cb) {
+        // do nothing with source ... not needed
+        cb();
+    };
+
+    flush = function (cb) {
+        var props,
+            process;
+
+        options = (typeof options === 'object') ? options : { sonar: {} };
+        options.sonar.language = options.sonar.language || 'js';
+        options.sonar.sourceEncoding = options.sonar.sourceEncoding || 'UTF-8';
+        options.sonar.host = options.sonar.host || { url: 'http://localhost:9000' };
+
+        function objectToProps(obj, result, prefix, o, prop) {
+            obj = (typeof obj === 'object') ? obj : {};
+            result = (typeof result === 'object' && typeof result.length === 'number') ? result : [];
+            prefix = (typeof prefix === 'string' && prefix.length) ? prefix + '.' : '';
+            for (o in obj) {
+                if (obj.hasOwnProperty(o)) {
+                    prop = prefix + o;
+                    if (typeof obj[o] === 'string') {
+                        result.push(prop + '=' + obj[o]);
+                    } else if (typeof obj[o] === 'object') {
+                        result = objectToProps(obj[o], result, prop);
+                    }
+                }
+            }
+            return result;
+        }
+
+        props = objectToProps(options);
+
+        fs.writeFile(path.join(SONAR_RUNNER_HOME, '/conf/sonar-runner.properties'), props.join(os.EOL), function (err) {
+            if (err) {
+                throw new PluginError('gulp-sonar', format('Error writing properties file: %d.', err));
+            } else {
+                process = exec(SONAR_RUNNER_COMMAND, function () {});
+                process.stdout.on('data', function (c) {
+                    gutil.log(c);
+                });
+                process.stderr.on('data', function (c) {
+                    gutil.log(c);
+                });
+                process.on('exit', function (code) {
+                    if (code !== 0) {
+                        gutil.log(format('Return code: %d.', code));
+                        throw new PluginError('gulp-sonar', format('Return code: %d.', code));
+                    }
+                    gutil.log(format('Return code: %d.', code));
+                    cb();
+                });
+            }
+        });
+
+    };
+
+    return through.obj(write, flush);
+
+};
